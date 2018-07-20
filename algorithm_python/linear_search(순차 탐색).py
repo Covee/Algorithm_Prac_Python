dbd977414bf201341533055e6d8f7d89b21c8a4d
Covee <uniqtop91@gmail.com>
2018-07-20 19:29:15 +0900

    순차 탐색, 빠진 조건 추가

--- algorithm_python/linear_search(순차 탐색).py
+++ algorithm_python/linear_search(순차 탐색).py
@@ -8,13 +8,14 @@ def find(nums, pick):
 	for i in range(0, len(nums)):
 		if nums[i] == pick:
 			print(str(pick) + "의 위치는 " + str(i+1) + "번째 자리에 있습니다.")
+			
+	return print("-1")
 
 
 n_list = list(map(int,input("리스트에 넣을 숫자들을 띄어쓰기로 구분해서 입력해 주세요: ").split(" ")))
 pick = int(input("찾을 숫자를 입력해 주세요: "))
 
 find(n_list, pick)
-
 
 
 # comment: 리스트에 5개 자료가 있으면 최대 5번을 돌아서 찾는 방식. 100만개 있으면... 엄청 비효율적이라는것을 알 수 있다.
