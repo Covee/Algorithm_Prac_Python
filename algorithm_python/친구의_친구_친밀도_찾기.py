573bbecfbcf5c9f83ed8f7b3411dee54c69ea7f9
Covee <uniqtop91@gmail.com>
2018-07-23 12:21:31 +0900

    출력을 약간 변경

--- algorithm_python/친구의_친구_친밀도_찾기.py
+++ algorithm_python/친구의_친구_친밀도_찾기.py
@@ -8,22 +8,24 @@
 def all_friend(db, person):
 	waiting = [] 		# 이제 처리할 사람들 리스트
 	done	= set() 	# 리스트와 달리 집합은 중복을 허용하지 않아 이미 처리한 사람은 여기 넣어서 중복을 방지한다.
-	result = set()
-
+	
 	waiting.append((person, 0))
 	done.add(person)
 
+	print(str(person) + "의 친구들은, \n-------------------")
+
 	while waiting:
 		(p, luv) = waiting.pop(0) 	# 처리할 리스트의 첫번째 친구와 친밀도를 꺼냄.
-		result.add((p, luv)) 		# print할 최종 결과를 튜플로 저장한다.
+		if not (p, luv) == (person, 0): 	# 자기 자신은 최종 결과에서 지움.
+			print(str(p)+ " ("+ str(luv) + "촌)")
+		# result.add((p, luv)) 		# print할 최종 결과를 튜플로 저장한다.
 		for n in db[p]:
 			if n not in done:
 				waiting.append((n, luv+1))
 				done.add(n)
 
-	result.remove((person, 0)) 	# 자기 자신은 최종 결과에서 지움.
+	print("-------------------\n입니다.")
 
-	return result
 
 
 friend_db = {
@@ -38,8 +40,7 @@ friend_db = {
 }
 
 person = input("누구의 모든 친구를 알고싶나요? ")
-
-print(str(person) + "는(은) " + str(all_friend(friend_db, person)) + "와 아는사이 입니다.")
+all_friend(friend_db, person)
 
 
 # comment: 알고리즘 구현할 때, 일단 쭉 말로 설명 가능하게끔 글로 풀어서 과정을 적어보고 각 과정에 맞는 코드를 생각해보면 좋음.
